edfce9dfcab01fd772e840387fb85cda440de9af
eughrat <michal.piernicki@gmail.com>
2022-05-17 22:20:49 +0200

    implementation of scaler, distance_between_points and random centroids functions

---
+++ Functions.py
@@ -0,0 +1,17 @@
+import numpy as np
+import random
+
+def scaler(data):
+
+    normalized_data = (data - min(data)) / (max(data) - min(data))
+    return normalized_data
+
+def distance_to_centroids(centroid, datapoint):
+
+    distance = np.sqrt(np.sum((centroid - datapoint) ** 2, axis=1))
+    return distance
+
+def random_centroids(data):
+
+    centroids = [random.choice(data)]
+    return centroids
